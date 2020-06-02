118422e983c69a21343508af06659f02fd5c90e9
Cesar kennedy rousseau arzapalo caldas <cesar.arzapalo@unmsm.edu.pe>
2020-06-02 18:58:10 -0500

    modificaciones del componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  contador: number;
   title = 'redux-app';
+
+  constructor(){
+    this.contador = 10;
+  }
 }
